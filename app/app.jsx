a5520c617cc56eb897c63fb070367d9d0ae022c8
Jeremy Lincoln <bossco28@gmail.com>
2016-06-08 14:44:15 -0400

    Fixed error with spread operator

--- app/app.jsx
+++ app/app.jsx
@@ -1,17 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var a = {
-  'name': 'jeremy'
-};
-
-var b = {
-  'age' : '34'
-  ...a
-};
-
-console.log(b);
-
 //this initialize the app
 ReactDOM.render(
 <h1>Boilerplate app</h1>,
